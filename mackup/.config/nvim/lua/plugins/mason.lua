911a2a373211aa1e4b4846d48b3fabe69210c68e
Turbo Thinh (NoA Ignite) <turbothinh@ni-turbo.local>
2023-07-10 11:05:29 +0200

    nvim: add ruby syntax highting

--- mackup/.config/nvim/lua/plugins/mason.lua
+++ mackup/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,11 @@
 return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
-    table.insert(opts.ensure_installed, "prettierd")
+    table.insert(opts.ensure_installed, {
+      "prettierd",
+    })
+    vim.list_extend(opts.ensure_installed, {
+      "solargraph",
+    })
   end,
 }
